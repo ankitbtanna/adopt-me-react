0ea004c714ea9904ef22dc68eac4de28d9e1cbca
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-09-10 12:49:00 +1000

    Add unique key to every element so that react warnings disappear
    - It's just like trackby in angular's ngFor

--- exercise-2-pure-components/src/App.js
+++ exercise-2-pure-components/src/App.js
@@ -1,17 +1,17 @@
 const Pet = () => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanese"),
+    React.createElement("h1", { key: 1 }, "Luna"),
+    React.createElement("h2", { key: 2 }, "Dog"),
+    React.createElement("h2", { key: 3 }, "Havanese"),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", { id: "myId" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement("h1", {key: "title-adopt-me" }, "Adopt Me!"),
+    React.createElement(Pet, {key: "pet-1" }),
+    React.createElement(Pet, {key: "pet-2" }),
+    React.createElement(Pet, {key: "pet-3" }),
   ]);
 };
 
