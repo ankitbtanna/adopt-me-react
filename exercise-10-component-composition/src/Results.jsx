c8b018900770f3674d078bf1d0a5611870ab9716
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-09-14 21:55:02 +1000

    component composition

---
+++ exercise-10-component-composition/src/Results.jsx
@@ -0,0 +1,27 @@
+import Pet from './Pet';
+
+function Results(props) {
+	const numberOfPets = props.pets.length;
+	const pets = props.pets;
+
+	return (
+		<div className='search'>
+			{numberOfPets === 0 ? (
+				<h1>No Pets found</h1>
+			) : (
+				pets.map((pet) => (
+					<Pet
+						animal={pet.animal}
+						name={pet.name}
+						breed={pet.breed}
+						images={pet.images}
+						location={`${pet.city}, ${pet.state}`}
+						key={pet.id}
+					/>
+				))
+			)}
+		</div>
+	);
+}
+
+export default Results;
