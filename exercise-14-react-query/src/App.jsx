06fb72f600fbe55ed24369894837bb2f188ccb26
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-09-20 22:11:09 +1000

    react query tutorial

---
+++ exercise-14-react-query/src/App.jsx
@@ -0,0 +1,28 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import SearchParams from './SearchParams';
+import Details from './Details';
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<header>
+				<Link to='/'>Adopt Me!</Link>
+			</header>
+			<Routes>
+				<Route
+					path='/'
+					element={<SearchParams />}
+				/>
+				<Route
+					path='/details/:id'
+					element={<Details />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
