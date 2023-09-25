f6b836bb2fe5417c968a37a2f4caacddd1e09d23
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-09-25 22:45:14 +1000

    uncontrolled forms in react

---
+++ exercise-16-uncontrolledForm/src/App.jsx
@@ -0,0 +1,41 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import SearchParams from './SearchParams';
+import Details from './Details';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: Infinity,
+			cacheTime: Infinity,
+			refetchOnWindowFocus: false,
+		},
+	},
+});
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<QueryClientProvider client={queryClient}>
+				<header>
+					<Link to='/'>Adopt Me!</Link>
+				</header>
+				<Routes>
+					<Route
+						path='/'
+						element={<SearchParams />}
+					/>
+					<Route
+						path='/details/:id'
+						element={<Details />}
+					/>
+				</Routes>
+			</QueryClientProvider>
+		</BrowserRouter>
+	);
+};
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
