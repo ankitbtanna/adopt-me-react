fd4104664f3e50781e61636431496588162083a7
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-09-27 13:56:54 +1000

    class component example

---
+++ exercise-17-class-components/src/Carousel.jsx
@@ -0,0 +1,54 @@
+import { Component } from 'react';
+import useBreedsList from './useBreedsList';
+
+class Carousel extends Component {
+	state = {
+		active: 0,
+	};
+
+	// if you dont give anything to the component as an input
+	static defaultProps = {
+		images: ['http://pets-images.dev-apis.com/pets/none.jpg'],
+	};
+
+	handleClickIndex = (event) => {
+		this.setState({
+			active: +event.target.dataset.index,
+		});
+	};
+
+	// the function body of the functional components
+	render() {
+		const { active } = this.state;
+		const { images } = this.props;
+
+		return (
+			<div className='carousel'>
+				<img
+					src={images[active]}
+					alt='animal'
+				/>
+				<div className='carousel-smaller'>
+					{images.map((photo, index) => (
+						// eslint-disable-next-line
+						<img
+							onClick={this.handleClickIndex}
+							data-index={index}
+							key={photo}
+							src={photo}
+							className={index === active ? 'active' : ''}
+							alt='animal thumbnail'
+						/>
+					))}
+				</div>
+			</div>
+		);
+	}
+}
+
+function CarouselParent({ animal }) {
+	const breedList = useBreedsList(animal);
+	return <Carousel breedList={breedList} />;
+}
+
+export default CarouselParent;
